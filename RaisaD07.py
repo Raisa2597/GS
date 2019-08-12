11c10315f2b33af418ac426d573b33152321b2d6
Raisa2597 <54040158+Raisa2597@users.noreply.github.com>
2019-08-12 15:18:14 -0400

    Add files via upload

---
+++ RaisaD07.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+created on Mon aug 12 11:24:34 2019
+This is a temporary script file.
+"""
+#(0719
+
+
+print('hello world')#ld09
+print("d0r7a09r1i9")
+print("raisa")
+      5*2
+5/2
+5-2
+5^2
+17+17
+def add(a,b):
+ add = a+b
+ print(add)
+ add(2,6)
+ add(10985.345,9802.472)
+ add(20419.087,069.112)
+ def adds(a,b,c):
+     adds = a+b+c
+     print(adds)
+      
+     adds(112,342,69)
